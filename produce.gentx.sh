62972317a73ac1ac34e8e4d385ba9813b062dd94
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-02 12:04:22 +0800

    Update produce.gentx.sh

--- produce.gentx.sh
+++ produce.gentx.sh
@@ -7,15 +7,15 @@ rm -rf ~/.okdecli
 
 ./recover.admin.sh
 
-okdexd init --chain-id okchain -o -v
+okdexd init --chain-id okchain
 
 # config okbcli
 okdexcli config chain-id okchain
 okdexcli config trust-node true
 okdexcli config indent true
 
-okdexd add-genesis-account cosmos1m3gmu4zlnv2hmqfu2jwr97r2653w9yshxkhfea 2okb
-okdexd gentx --amount 1okb --min-self-delegation 1 --commission-rate 0.1 --commission-max-rate 0.5 --commission-max-change-rate 0.001 --pubkey $(okdexd tendermint show-validator) --name admin
+okdexd add-genesis-account cosmos1m3gmu4zlnv2hmqfu2jwr97r2653w9yshxkhfea 2000000okb
+okdexd gentx --amount 1000000okb --min-self-delegation 1 --commission-rate 0.1 --commission-max-rate 0.5 --commission-max-change-rate 0.001 --pubkey $(okdexd tendermint show-validator) --name admin
 rm gentx/data/gentx-*
 cp ~/.okdexd/config/gentx/gentx-*.json gentx/data
 
