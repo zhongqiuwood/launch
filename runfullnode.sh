a2f9977a73904c8721b1c0b2f25b4bc8f297561d
zhongqiuwood <zhongqiuwood@gmail.com>
2019-04-02 16:13:22 +0800

    upd

--- runfullnode.sh
+++ runfullnode.sh
@@ -11,4 +11,4 @@ scp root@192.168.13.116:/root/go/src/github.com/cosmos/launch/genesis.json .
 
 cp genesis.json ~/.okdexd/config
 
-okdexd start --p2p.seeds $1@192.168.13.116:26656 --p2p.addr_book_strict=false --log_level *:info --p2p.laddr tcp://192.168.26.129:26656
+okdexd start --p2p.seeds $1@192.168.13.116:26656 --p2p.addr_book_strict=false --log_level *:info --p2p.laddr tcp://$2:26656
