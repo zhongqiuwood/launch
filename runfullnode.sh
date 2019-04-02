602e122559bb37e7bdafff2651424097450e4f4e
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-02 16:04:51 +0800

    create validator

--- runfullnode.sh
+++ runfullnode.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 rm -rf ~/.okdexd
+rm -rf ~/.okdexcli
 
 ./killbyname.sh okdexd
 
-okdexd init --chain-id okchain -o -v
+okdexd init --chain-id okchain
 
 scp root@192.168.13.116:/root/go/src/github.com/cosmos/launch/genesis.json .
 
 cp genesis.json ~/.okdexd/config
 
-okdexd start --p2p.seeds $1@192.168.13.116:26656 --p2p.addr_book_strict=false --log_level *:debug
+okdexd start --p2p.seeds $1@192.168.13.116:26656 --p2p.addr_book_strict=false --log_level *:info --p2p.laddr tcp://192.168.26.129:26656
--- set.genesis.and.start.sh
+++ set.genesis.and.start.sh
@@ -3,4 +3,4 @@
 
 cp genesis.json ~/.okdexd/config
 
-okdexd start --p2p.seed_mode=true --p2p.addr_book_strict=false --log_level *:debug
+okdexd start --p2p.seed_mode=true --p2p.addr_book_strict=false --log_level *:info --p2p.laddr tcp://192.168.13.116:26656
