59335f3da0846be61adfe522e273e61b17212f7c
zhongqiuwood <zhongqiuwood@gmail.com>
2019-04-08 07:37:08 +0800

    upd

--- systemctl/fullnode/okdexd_start.sh
+++ systemctl/fullnode/okdexd_start.sh
@@ -6,7 +6,7 @@ source /root/go/src/github.com/cosmos/launch/systemctl/fullnode/okdexd.profile
 LOCAL_IP=`ifconfig  | grep 192.168 | awk '{print $2}' | cut -d: -f2`
 
 
-scp root@${SEED_NODE_IP}:/root/go/src/github.com/cosmos/launch/genesis.json ~/.okdexd/config
+scp root@${SEED_NODE_IP}:/root/.okdexd/config/genesis.json ~/.okdexd/config
 
 /root/go/bin/okdexd start --home /root/.okdexd \
     --p2p.seeds ${SEED_NODE_ID}@${SEED_NODE_URL} \
