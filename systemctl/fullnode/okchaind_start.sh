ea0b43ddc5f4df6e016fa5d9925de21f45963a72
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-17 20:07:27 +0800

    Update okchaind_start.sh

--- systemctl/fullnode/okchaind_start.sh
+++ systemctl/fullnode/okchaind_start.sh
@@ -9,10 +9,11 @@ scp root@${SEED_NODE_IP}:/root/go/src/github.com/cosmos/launch/systemctl/seednod
 
 LOCAL_IP=`ifconfig  | grep 192.168 | awk '{print $2}' | cut -d: -f2`
 
-host="okchain"${LOCAL_IP:0-2:2}
-scp -r root@${SEED_NODE_IP}:/root/.okchaind/${host}/ /root/.okchaind/
-scp -r root@${SEED_NODE_IP}:/root/.okchaincli/${host}/ /root/.okchaincli/
-  
+if [ ! -d /root/.okchaind ]; then
+    host="okchain"${LOCAL_IP:0-2:2}
+    scp -r root@${SEED_NODE_IP}:/root/.okchaind/${host}/ /root/.okchaind/
+    scp -r root@${SEED_NODE_IP}:/root/.okchaincli/${host}/ /root/.okchaincli/
+fi
 /usr/local/go/bin/okchaind start --home /root/.okchaind \
     --p2p.seeds ${SEED_NODE_ID}@${SEED_NODE_URL} \
     --p2p.addr_book_strict=false \
