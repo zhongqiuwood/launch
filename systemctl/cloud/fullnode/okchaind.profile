6b50d8cb81a793a7117c5e122572551adaf6e14e
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-22 10:22:06 +0800

    upd

--- systemctl/cloud/fullnode/okchaind.profile
+++ systemctl/cloud/fullnode/okchaind.profile
@@ -7,8 +7,8 @@ HOME_CLI=/home/ubuntu/.okchaincli
 OKCHAIN_LAUNCH_TOP=/home/ubuntu/okchain/launch
 
 # 是否使用内网IP 如果false，则使用公网IP；如果是true，则必须设置IP_PREFIX
-IP_INNET=false
-IP_PREFIX=192.168
+IP_INNET=true
+IP_PREFIX=172.31
 HOSTS_PREFIX=okchain_cloud
 
-SEED_NODE_IP=18.179.58.10
+SEED_NODE_IP=172.31.26.8
--- systemctl/testnet_remote/setseed.sh
+++ systemctl/testnet_remote/setseed.sh
@@ -6,8 +6,8 @@ function main {
     seedid=$(${OKCHAIN_DAEMON} tendermint show-node-id --home ${HOME_DAEMON})
     cat>${OKCHAIN_LAUNCH_TOP}/systemctl/cloud/seednode/seednode.profile<<EOF
 SEED_NODE_ID=${seedid}
-SEED_NODE_IP=${OKCHAIN_TESTNET_SEED_HOST}
-SEED_NODE_URL=${OKCHAIN_TESTNET_SEED_HOST}:26656
+SEED_NODE_IP=${SEED_NODE_IP}
+SEED_NODE_URL=${SEED_NODE_IP}:26656
 EOF
 }
 
