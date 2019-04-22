2f87765acca6fbff8846999179e1c10856aed08c
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-22 17:01:00 +0800

    Update okchaind_kill.sh

--- systemctl/okchaind_kill.sh
+++ systemctl/okchaind_kill.sh
@@ -4,7 +4,7 @@ NAME=okchaind
 MYNAME="okchaind_kill.sh"
 
 ps -ef|grep "$NAME"|grep -v grep |grep -v $MYNAME |awk '{print "kill -9 "$2", "$8}'
-ps -ef|grep "$NAME"|grep -v grep |grep -v $MYNAME |awk '{print "kill -9 "$2}' | sh
+ps -ef|grep "$NAME"|grep -v grep |grep -v $MYNAME |awk '{print "kill -9 "$2}' | sudo sh
 echo "All <$NAME> killed!"
 
 
