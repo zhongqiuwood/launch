26a22c2c4e15e6abbfa05e5265d87374c307603b
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-22 11:52:20 +0800

    Update setseed.sh

--- systemctl/testnet_remote/setseed.sh
+++ systemctl/testnet_remote/setseed.sh
@@ -4,7 +4,7 @@
 
 function main {
     seedid=$(${OKCHAIN_DAEMON} tendermint show-node-id --home ${HOME_DAEMON})
-    cat>${OKCHAIN_LAUNCH_TOP}/systemctl/cloud/seednode/seednode.profile<<EOF
+    cat>${OKCHAIN_LAUNCH_TOP}/systemctl/cloud/profile/seednode.profile<<EOF
 SEED_NODE_ID=${seedid}
 SEED_NODE_IP=${SEED_NODE_IP}
 SEED_NODE_URL=${SEED_NODE_IP}:26656
