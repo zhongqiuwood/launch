75227a8e640a8eda238c744c823861415823a2c7
linsheng.yu@okcoin.com <linsheng.yu@okcoin.com>
2019-04-23 13:44:46 +0800

    Update clean.sh

--- systemctl/scripts/clean.sh
+++ systemctl/scripts/clean.sh
@@ -2,7 +2,7 @@
 
 . ./okchaind.profile
 
-# rm -rf ${HOME_DAEMON}
-# rm -rf ${HOME_CLI}
-${OKCHAIN_DAEMON} unsafe-reset-all
+rm -rf ${HOME_DAEMON}
+rm -rf ${HOME_CLI}
+# ${OKCHAIN_DAEMON} unsafe-reset-all
 rm -f ${HOME}/okchaind.log
