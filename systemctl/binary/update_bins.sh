43274a48434f4f123bf648dc2440a332fdda0ba8
zhongqiuwood <zhongqiuwood@gmail.com>
2019-04-27 07:34:52 +0800

    upd

--- systemctl/binary/update_bins.sh
+++ systemctl/binary/update_bins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function pull_build {
+function push_build {
 ssh root@192.168.13.116 << eeooff
     source ~/env.sh
     cd /root/go/src/github.com/ok-chain/okchain
@@ -22,21 +22,9 @@ eeooff
 echo done!
 }
 
-function getbinary {
-    scp root@192.168.13.116:/usr/local/go/bin/okchaind ~/go/src/github.com/cosmos/launch/systemctl/binary/
-    scp root@192.168.13.116:/usr/local/go/bin/okchaincli  ~/go/src/github.com/cosmos/launch/systemctl/binary/
-    scp root@192.168.13.116:/root/go/src/github.com/cosmos/launch/launch  ~/go/src/github.com/cosmos/launch/systemctl/binary/
-
-    gzip -f ~/go/src/github.com/cosmos/launch/systemctl/binary/okchaind
-    gzip -f ~/go/src/github.com/cosmos/launch/systemctl/binary/okchaincli
-    gzip -f ~/go/src/github.com/cosmos/launch/systemctl/binary/launch
-
-#    cp -r ~/go/src/github.com/cosmos/launch/systemctl/binary ~/go/launch/systemctl/
-}
 
 function main {
-    pull_build
-#    getbinary
+    push_build
 }
 
 main
