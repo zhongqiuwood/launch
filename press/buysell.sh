6336dba78259a6e618b98aa66abe993280584317
zhongqiuwood <zhongqiuwood@gmail.com>
2019-04-28 17:12:56 +0800

    perf log

--- press/buysell.sh
+++ press/buysell.sh
@@ -5,10 +5,15 @@ for((;;)) do
     ./mint.sh 10000
     ./reward.sh
     ./order.sh -c 10 -x 10 -b 10 -d 50 -p 0.1 -s
+    ./order.sh -p 5 -q 10 -u captain -c 1 -x 1 -b 1 -d 1
     ./order.sh -c 10 -x 10 -b 10 -d 50 -p 10
+    ./order.sh -p 5 -q 10 -u captain -c 1 -x 1 -b 1 -d 1
     ./order.sh -c 10 -x 10 -b 10 -d 50 -p 0.1 -s
+    ./order.sh -p 5 -q 10 -u captain -c 1 -x 1 -b 1 -d 1
     ./order.sh -c 10 -x 10 -b 10 -d 50 -p 10
+    ./order.sh -p 5 -q 10 -u captain -c 1 -x 1 -b 1 -d 1
     ./order.sh -c 10 -x 10 -b 10 -d 50 -p 0.1 -s
+    ./order.sh -p 5 -q 10 -u captain -c 1 -x 1 -b 1 -d 1
     ./order.sh -c 10 -x 10 -b 10 -d 50 -p 10
 done
 
