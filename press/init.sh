81685a075cde5811a40b9d58919048470561827d
zhongqiuwood <zhongqiuwood@gmail.com>
2019-05-01 22:39:46 +0800

    upd

--- press/init.sh
+++ press/init.sh
@@ -5,4 +5,4 @@ CURDIR=`dirname $0`
 . ${CURDIR}/../systemctl/testnet_remote/token.profile
 
 # 16 concurrent/admins, each 1000000 coins
-./reward.sh -R -n c22 -b 10000000 -c 16 -u admin
+./reward.sh -R -n c22 -b 10000000 -c 16 -u admin -h admin_home/admin
