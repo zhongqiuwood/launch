d93d862963d27961003c826201cc99707d3a1c8a
zhongqiuwood <zhongqiuwood@gmail.com>
2019-05-02 11:23:49 +0800

    upd

--- press/query.sh
+++ press/query.sh
@@ -8,5 +8,6 @@ okecho() {
 token=$1
 index=$2
 
-okecho okchaincli query account $(okchaincli keys show user$index --home ./products/$token_okb/$token_okb$index -a) --node c22:26657
-
+okecho okchaincli keys show user$index --home ./products/${token}_okb/${token}_okb${index} -a
+addr=$(okchaincli keys show user$index --home ./products/${token}_okb/${token}_okb${index} -a)
+okecho okchaincli query account $addr --node c22:26657
